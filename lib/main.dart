d46af15a3c9b5e273f9afca9bd736bd8d05b70c3
Andres Delgado Varela <andredel357@gmail.com>
2024-12-03 20:13:39 -0500

    Commit incial del proyecto poke_app

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PokeApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Map<String, dynamic>> fetchPokemon() async {
+    final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/1'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load Pokemon');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PokeApp'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: fetchPokemon(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final pokemon = snapshot.data!;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Name: ${pokemon['name']}'),
+                  Image.network(pokemon['sprites']['front_default']),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
